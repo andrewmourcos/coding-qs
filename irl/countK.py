af594397536e8ee69f5b416c7e0117439aa4c00a
Andrew Mourcos <amourcos@gmail.com>
2020-01-04 23:22:00 -0500

    started on countKs

--- irl/countK.py
+++ irl/countK.py
@@ -6,3 +6,13 @@ ex:
 	return: 4
 	reason: there are 4 "threes" in the list
 """
+
+def getLeftBound(L, k):
+	mid = int( len(L)/2 )
+
+	if (L[mid] > k):
+		getLeftBound(L[mid:], k)
+	elif (L[mid] < k):
+		getLeftBound(L[:mid], k)
+	else:
+		return mid
