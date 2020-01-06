05b0a63511bf3070bdef2f01e114a674bf4320e1
Andrew Mourcos <amourcos@gmail.com>
2020-01-05 21:59:02 -0500

    continued irl

--- irl/countK.py
+++ irl/countK.py
@@ -7,12 +7,26 @@ ex:
 	reason: there are 4 "threes" in the list
 """
 
-def getLeftBound(L, k):
-	mid = int( len(L)/2 )
+def getLeftBound(L, k, mid_sum):
+	if len(L) <= 1:
+		print("not found")
+		return 
+
+	mid = len(L)//2 
+	print(L, mid, mid_sum)
 
 	if (L[mid] > k):
-		getLeftBound(L[mid:], k)
+		# go left
+		return getLeftBound(L[:mid], k, mid_sum+mid)
 	elif (L[mid] < k):
-		getLeftBound(L[:mid], k)
+		# go right
+		return getLeftBound(L[mid:], k, mid_sum+mid)
 	else:
 		return mid
+
+
+if __name__ == '__main__':
+	L = [1,2,3,3,3,4,5,6,6,6,6,6,6,6,6,6]
+	k = 5
+	i = getLeftBound(L, k, 0)
+	print(i)
